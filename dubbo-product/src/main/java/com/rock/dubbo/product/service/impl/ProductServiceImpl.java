da4a3ecc9667022d29cba93f4ec998749bf28392
Rock-Ayl <AnYongLiang4869@163.com>
2024-10-30 23:55:16 +0800

    修改：完善基础dubbo设施-15

--- dubbo-product/src/main/java/com/rock/dubbo/product/service/impl/ProductServiceImpl.java
+++ dubbo-product/src/main/java/com/rock/dubbo/product/service/impl/ProductServiceImpl.java
@@ -3,14 +3,14 @@ package com.rock.dubbo.product.service.impl;
 import com.rock.dubbo.api.user.UserRemoteService;
 import com.rock.dubbo.product.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    @Reference(interfaceClass = UserRemoteService.class, version = "1.0.0")
+    @DubboReference
     private UserRemoteService userRemoteService;
 
     public String getUserData() {
--- dubbo-user/src/main/java/com/rock/dubbo/user/interfaces/UserRemoteServiceImpl.java
+++ dubbo-user/src/main/java/com/rock/dubbo/user/interfaces/UserRemoteServiceImpl.java
@@ -2,12 +2,12 @@ package com.rock.dubbo.user.interfaces;
 
 import com.rock.dubbo.api.user.UserRemoteService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Service(version = "1.0.0", timeout = 6000)
 @Component
+@DubboService
 public class UserRemoteServiceImpl implements UserRemoteService {
 
     /**
