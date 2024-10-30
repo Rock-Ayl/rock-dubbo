77c6b83120b85d090ea05fa61aff0394e6cea7bd
Rock-Ayl <AnYongLiang4869@163.com>
2024-10-30 16:04:21 +0800

    修改：完善基础dubbo设施-1

---
+++ dubbo-user-api/src/main/java/UserServiceAPI.java
@@ -0,0 +1,16 @@
+/**
+ * 用户服务远程接口
+ *
+ * @Author ayl
+ * @Date 2024-10-30
+ */
+public interface UserServiceAPI {
+
+    /**
+     * 测试
+     *
+     * @return
+     */
+    String helloWorld();
+
+}
