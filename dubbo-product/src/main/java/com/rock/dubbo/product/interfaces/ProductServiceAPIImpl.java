1c1ba4be8133f1b4f20c670be581dad8572be52e
Rock-Ayl <AnYongLiang4869@163.com>
2024-10-30 16:34:43 +0800

    修改：完善基础dubbo设施-7

---
+++ dubbo-product/src/main/java/com/rock/dubbo/product/interfaces/ProductServiceAPIImpl.java
@@ -0,0 +1,22 @@
+package com.rock.dubbo.product.interfaces;
+
+import com.rock.dubbo.api.product.ProductServiceAPI;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Service(version = "1.0.0", timeout = 6000)
+@Component
+public class ProductServiceAPIImpl implements ProductServiceAPI {
+
+    /**
+     * 实现
+     *
+     * @return
+     */
+    public String helloWorld() {
+        return "你获得了产品数据!";
+    }
+
+}
