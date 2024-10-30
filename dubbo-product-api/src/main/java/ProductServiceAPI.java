b7a51c61ede901b225e3c1f48cfea3d0fa82aeae
Rock-Ayl <AnYongLiang4869@163.com>
2024-10-30 16:06:07 +0800

    修改：完善基础dubbo设施-3

---
+++ dubbo-product-api/src/main/java/ProductServiceAPI.java
@@ -0,0 +1,16 @@
+/**
+ * 产品服务远程接口
+ *
+ * @Author ayl
+ * @Date 2024-10-30
+ */
+public interface ProductServiceAPI {
+
+    /**
+     * 测试
+     *
+     * @return
+     */
+    String helloWorld();
+
+}
