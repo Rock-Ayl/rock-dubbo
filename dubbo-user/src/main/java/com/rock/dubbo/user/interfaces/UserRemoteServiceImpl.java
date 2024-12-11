dffb5fd7705d36ea777e686b44431ba25434c869
Rock-Ayl <AnYongLiang4869@163.com>
2024-12-11 12:03:50 +0800

    修改：增加了nacos的 配置中心实现

--- dubbo-user/src/main/java/com/rock/dubbo/user/interfaces/UserRemoteServiceImpl.java
+++ dubbo-user/src/main/java/com/rock/dubbo/user/interfaces/UserRemoteServiceImpl.java
@@ -3,12 +3,24 @@ package com.rock.dubbo.user.interfaces;
 import com.rock.dubbo.api.user.UserRemoteService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+//该注解,用于指定dubbo服务实现
 @DubboService
+//该注解,用于自动刷新@Value,如果nacos配置中心重新发布,会自动刷新
+@RefreshScope
 public class UserRemoteServiceImpl implements UserRemoteService {
+
+    /**
+     * 用来测试 nacos 作为配置中心,如果开启,一定要有对应的配置中心-发布配置
+     * 注意,配置的dataId,要与 bootstrap.properties 中的 {spring.application.name} 保持一致
+     */
+    //@Value(value = "${nacos.config.testConfigCenter}")
+    private String testConfigCenter;
 
     /**
      * 实现
